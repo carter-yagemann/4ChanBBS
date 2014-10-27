eb176184d91f975d8acf4787fa0cacefecad74f1
Carter Yagemann <sector892@gmail.com>
2014-10-27 15:27:10 -0400

    Added offline mode for development.

--- chanjson.py
+++ chanjson.py
@@ -1,17 +1,24 @@
 ## Imports
 ##------------------------------------------------------
 import json, requests
+from config import config
+if (config.offline_mode):
+    import test_data
 
 class ChanServer:
     
     # Gets all the boards
     def getBoards(self):
+        if (config.offline_mode):
+            return json.loads(test_data.boards_list)
         response = requests.get(url='https://a.4cdn.org/boards.json')
         data = json.loads(response.text)
         return data
 
     # Get a page of threads from a board
     def getThreads(self, board, page):
+        if (config.offline_mode):
+            return json.loads(test_data.threads_list)
         try:
             response = requests.get(url='https://a.4cdn.org/' + board + '/' + str(page) + '.json')
             data = json.loads(response.text)
@@ -22,6 +29,8 @@ class ChanServer:
 
     # Get posts for a particular thread
     def getReplies(self, board, thread):
+        if (config.offline_mode):
+            return json.loads(test_data.replies_list)
         try:
             response = requests.get(url='https://a.4cdn.org/' + board + '/thread/' + str(thread) + '.json')
             data = json.loads(response.text)
--- config.py
+++ config.py
@@ -35,3 +35,7 @@ If you need additional help or examples, try help <command>.
     ##-----------------------------------
     server = "0.0.0.0"
     port = 5000
+
+    ## OFFLINE DEVELOPMENT MODE
+    ##-----------------------------------
+    offline_mode = False
