d552e89f11a973bda1ad017367713d7078f00a54
Carter Yagemann <carter.yagemann@gmail.com>
2014-10-27 08:16:20 -0400

    Improved HTML parser.

--- htmlcleaner.py
+++ htmlcleaner.py
@@ -20,6 +20,32 @@ class MLStripper(HTMLParser):
             self.fed.append('>')
         elif (name == 'amp'):
             self.fed.append('&')
+        elif (name == 'excl'):
+            self.fed.append('!')
+        elif (name == 'quot'):
+            self.fed.append('"')
+        elif (name == 'num'):
+            self.fed.append('#')
+        elif (name == 'percnt'):
+            self.fed.append('%')
+        elif (name == 'apos'):
+            self.fed.append('\'')
+        elif (name == 'lpar'):
+            self.fed.append('(')
+        elif (name == 'rpar'):
+            self.fed.append(')')
+        elif (name == 'comma'):
+            self.fed.append(',')
+        elif (name == 'period'):
+            self.fed.append('.')
+        elif (name == 'lsqb'):
+            self.fed.append('[')
+        elif (name == 'lbrack'):
+            self.fed.append('[')
+        elif (name == 'rsqb'):
+            self.fed.append(']')
+        elif (name == 'rbrack'):
+            self.fed.append(']')
 
 def strip_tags(html):
     s = MLStripper()
