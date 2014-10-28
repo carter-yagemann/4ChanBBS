a1523fe97c9a9e808a7cdd5d3add01d590a3ec01
Carter Yagemann <sector892@gmail.com>
2014-10-28 10:38:40 -0400

    Added license.

--- ascii_image.py
+++ ascii_image.py
@@ -1,3 +1,22 @@
+#
+#    4Chan BBS
+#    ASCII Image - ascii_image.py
+#
+#    Copyright Carter Yagemann 2014
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    See <http://www.gnu.org/licenses/> for a full copy of the license.
+#
+
 from PIL import Image
 import urllib, cStringIO
 
