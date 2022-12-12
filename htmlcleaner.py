4d4734f75531555ec692b28f9a0933b3b265125b
adrenen <)>
2022-12-12 16:08:29 -0500

    updated to use html.parser

--- htmlcleaner.py
+++ htmlcleaner.py
@@ -17,54 +17,51 @@
 #    See <http://www.gnu.org/licenses/> for a full copy of the license.
 #
 
-from HTMLParser import HTMLParser
+from html.parser import HTMLParser
+
 
 class MLStripper(HTMLParser):
     def __init__(self):
+        super().__init__()
         self.reset()
         self.fed = []
-    def handle_data(self, d):
-        self.fed.append(d)
+
+        self.entities = {
+            'nbsp': ' ',
+            'lt': '<',
+            'gt': '>',
+            'amp': '&',
+            'excl': '!',
+            'quot': '"',
+            'num': '#',
+            'percnt': '%',
+            'apos': '\'',
+            'lpar': '(',
+            'rpar': ')',
+            'comma': ',',
+            'period': '.',
+            'lsqb': '[',
+            'lbrack': '[',
+            'rsqb': ']',
+            'rback': ']'
+        }
+
+    def handle_data(self, data):
+        self.fed.append(data)
+
     def get_data(self):
         return ''.join(self.fed)
+
     def handle_starttag(self, tag, attrs):
-        if (tag == 'br'):
+        if tag == 'br':
             self.fed.append("\n")
+
+    def handle_charref(self, name):
+        self.fed.append(self.entities.get(name, ''))
+
     def handle_entityref(self, name):
-        if (name == 'nbsp'):
-            self.fed.append(' ')
-        elif (name == 'lt'):
-            self.fed.append('<')
-        elif (name == 'gt'):
-            self.fed.append('>')
-        elif (name == 'amp'):
-            self.fed.append('&')
-        elif (name == 'excl'):
-            self.fed.append('!')
-        elif (name == 'quot'):
-            self.fed.append('"')
-        elif (name == 'num'):
-            self.fed.append('#')
-        elif (name == 'percnt'):
-            self.fed.append('%')
-        elif (name == 'apos'):
-            self.fed.append('\'')
-        elif (name == 'lpar'):
-            self.fed.append('(')
-        elif (name == 'rpar'):
-            self.fed.append(')')
-        elif (name == 'comma'):
-            self.fed.append(',')
-        elif (name == 'period'):
-            self.fed.append('.')
-        elif (name == 'lsqb'):
-            self.fed.append('[')
-        elif (name == 'lbrack'):
-            self.fed.append('[')
-        elif (name == 'rsqb'):
-            self.fed.append(']')
-        elif (name == 'rbrack'):
-            self.fed.append(']')
+        self.fed.append(self.entities.get(name, ''))
+
 
 def strip_tags(html):
     s = MLStripper()
